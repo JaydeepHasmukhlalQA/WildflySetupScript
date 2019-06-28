f325cc75834838d1bf7f172faa5b3543944ae49b
Jaydeep Hasmukhlal <jaydeep.hasmukhlal@academytrainee.com>
2019-06-28 13:15:23 +0100

    Fixed an error where service would not run.

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ sudo usermod --shell /bin/bash wildfly
 
 echo "Downloading Wildfly"
 sudo wget https://download.jboss.org/wildfly/17.0.0.Final/wildfly-17.0.0.Final.tar.gz
+
+echo "Extracting Wildfly"
 sudo tar -xvf wildfly-17.0.0.Final.tar.gz
 sudo rm -f wildfly-17.0.0.Final.tar.gz
 
-sudo mv ./wildfly-17.0.0.Final /home/wildfly
+sudo mv ./wildfly-17.0.0.Final /home/wildflyFinal
 
 echo "Installing Wildfly Service Script"
 sudo cp ./wildfly.service /etc/systemd/system/
